7a3edc769b545d4d7e106bf1c961ce78c569fcc0
Anne Marie Bogar <abogar@fordham.edu>
2024-09-26 18:04:59 +0200

    Added third endpoint
    Added endpoint to count most used words in all comments

--- data_aggregator_api.py
+++ data_aggregator_api.py
@@ -6,33 +6,58 @@ from collections import Counter
 
 app = Flask(__name__)
 
+def clean_text(comment_text, punctuation=True):
+    comment_text = comment_text.replace("&quot;", " ").replace("<p>", " ").replace("&#x27;", " ")
+    if not punctuation:
+        comment_text = re.sub(r'[^\w\s]', '', comment_text)
+    return comment_text
+
 def get_top_stories(num):
     response = requests.get('https://hacker-news.firebaseio.com/v0/topstories.json').json()[:num]
     return response
 
 def get_comment_list(sID):
-    comments = requests.get(f'https://hacker-news.firebaseio.com/v0/item/{sID}.json').json()
-    if 'kids' in comments.keys():
-        return comments['kids']
-    return []
+    story = requests.get(f'https://hacker-news.firebaseio.com/v0/item/{sID}.json').json()
+    #if 'kids' in story.keys():
+    #    return story['kids']
+    #return []
+    return story.get('kids', [])
 
 def get_comment(cID, punctuation=True):
-    comment_text = requests.get(f'https://hacker-news.firebaseio.com/v0/item/{cID}.json').json()['text']
-    comment_text = re.sub(r'&quot;', ' ', comment_text)
-    comment_text = re.sub(r'<p>', ' ', comment_text)
-    comment_text = re.sub(r'&#x27;', '\'', comment_text)
-    if not punctuation:
-        comment_text = re.sub(r'[^\w\s]', '', comment_text)
+    comment = requests.get(f'https://hacker-news.firebaseio.com/v0/item/{cID}.json').json()
+    comment_text = clean_text(comment.get('text', ''), punctuation)
     return comment_text
 
-def get_agg_comment_list(storyLim, commentLim):
+def get_agg_comment_list(storyLim, commentLim=0):
     comment_ids = []
     stories = get_top_stories(storyLim)
     for s in stories:
-        if len(comment_ids) >= commentLim:
+        if commentLim > 0 and len(comment_ids) >= commentLim:
             break
         comment_ids += get_comment_list(int(s))
     return comment_ids
+
+def recurse_comment_list(cID):
+    comments = []
+    comment = requests.get(f'https://hacker-news.firebaseio.com/v0/item/{cID}.json').json()
+    if 'kids' in comment.keys():
+        for k in comment['kids']:
+            comments += recurse_comment_list(k)
+    if 'text' in comment.keys():
+        comment_text = clean_text(comment['text'], False)
+        comments += comment_text.split()
+    return comments
+
+def get_most_common_words(comment_ids, commentLim, wordLim):
+    comments = []
+    count = 0
+    for c in comment_ids:
+        if count >=commentLim:
+            break
+        comments += get_comment(c, False).split()
+        count += 1
+    c = Counter(comments).most_common(wordLim)
+    return c
 
 @app.route("/50comments", methods=['GET'])
 def get_50_comments():
@@ -42,18 +67,17 @@ def get_50_comments():
 
 @app.route("/10mostusedwords", methods=['GET'])
 def get_10_most_used_words():
-    comments = []
-    count = 0
     comment_ids = get_agg_comment_list(30, 100)
-    for c in comment_ids:
-        if count >=100:
-            break
-        comments += get_comment(c, True).split()
-        count += 1
-    c = Counter(comments).most_common(10)
+    c = get_most_common_words(comment_ids, 100, 10)
     return jsonify([word[0] for word in c])
 
-#@app.route("/mostusedwords", methods=['GET'])
+@app.route("/mostusedwords", methods=['GET'])
+def get_most_used_words_all_comments():
+    comment_ids = get_agg_comment_list(10)
+    for c in comment_ids:
+        comments = recurse_comment_list(c)
+    c = Counter(comments).most_common(25)
+    return jsonify([word[0] for word in c])
 
 if __name__=='__main__':
     app.run()
