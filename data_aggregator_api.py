c2c9d0499867b9302312da71f9bf4e8b38d8afc6
Anne Marie Bogar <abogar@fordham.edu>
2024-09-26 16:44:20 +0200

    Added second endpoint
    Second endpoint returns ten most common words in top 100 comments

--- data_aggregator_api.py
+++ data_aggregator_api.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import datetime
 import requests
 import re
+from collections import Counter
 
 app = Flask(__name__)
 
@@ -12,31 +13,45 @@ def get_top_stories(num):
 def get_comment_list(sID):
     comments = requests.get(f'https://hacker-news.firebaseio.com/v0/item/{sID}.json').json()
     if 'kids' in comments.keys():
-        if len(comments['kids']) <=50:
-            return comments['kids']
-        return comments['kids'][:50]
+        return comments['kids']
     return []
 
-def get_comment(cID):
+def get_comment(cID, punctuation=True):
     comment_text = requests.get(f'https://hacker-news.firebaseio.com/v0/item/{cID}.json').json()['text']
     comment_text = re.sub(r'&quot;', ' ', comment_text)
     comment_text = re.sub(r'<p>', ' ', comment_text)
     comment_text = re.sub(r'&#x27;', '\'', comment_text)
+    if not punctuation:
+        comment_text = re.sub(r'[^\w\s]', '', comment_text)
     return comment_text
+
+def get_agg_comment_list(storyLim, commentLim):
+    comment_ids = []
+    stories = get_top_stories(storyLim)
+    for s in stories:
+        if len(comment_ids) >= commentLim:
+            break
+        comment_ids += get_comment_list(int(s))
+    return comment_ids
 
 @app.route("/50comments", methods=['GET'])
 def get_50_comments():
-    comment_ids = []
-    comments = []
-    stories = get_top_stories(100)
-    for s in stories:
-        if len(comment_ids) >= 50:
-            break
-        comment_ids += get_comment_list(int(s))
+    comment_ids = get_agg_comment_list(100, 50)
     comments = [get_comment(c) for i,c in enumerate(comment_ids) if i<50]
     return jsonify(comments)
 
-#@app.route("/10mostusedwords", methods=['GET'])
+@app.route("/10mostusedwords", methods=['GET'])
+def get_10_most_used_words():
+    comments = []
+    count = 0
+    comment_ids = get_agg_comment_list(30, 100)
+    for c in comment_ids:
+        if count >=100:
+            break
+        comments += get_comment(c, True).split()
+        count += 1
+    c = Counter(comments).most_common(10)
+    return jsonify([word[0] for word in c])
 
 #@app.route("/mostusedwords", methods=['GET'])
 
